89f9a1c483815bb2e33bfff57ec4b338eed0f223
vanyaxromow <vanya.xromow@yandex.ru>
2024-06-26 12:31:14 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+count_homework = 12
+count_time = 1.5
+course_name = 'Pyton'
+one_lessons_time = count_time / count_homework
+print('Курс:', course_name + ',', 'всего задач:', str(count_homework) + ',', 'затрачено часов:', str(count_time) + ',', 'среднее время выполнения', one_lessons_time, 'часа')
